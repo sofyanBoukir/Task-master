7cedc3309ec5a40dd3c4cf58aff481b6dc2aa000
Soufian dev <soufianeboukir0@gmail.com>
2024-12-08 12:31:32 +0100

    frontend fix issues

--- front-end/src/components/layout/Header.jsx
+++ front-end/src/components/layout/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <div className="bg-white py-2 px-6 pl-20 w-[100%] fixed z-10">
         <p className="font-semibold text-lg">👋Hello, {user ? user.full_name : "user"}</p>
-        <p className="text-sm">You have 3 projects</p>
+        <p className="text-sm">Welcome to your dashboard</p>
     </div>
   )
 }
--- front-end/src/components/layout/Profile.jsx
+++ front-end/src/components/layout/Profile.jsx
@@ -94,7 +94,7 @@ export const Profile = () => {
             <svg className="rotate-[135deg] size-full" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
               <circle cx="18" cy="18" r="16" fill="none" className="stroke-current text-gray-200 dark:text-neutral-700" stroke-width="1.5" stroke-dasharray="100 100" stroke-linecap="round"></circle>
 
-              <circle cx="18" cy="18" r="16" fill="none" className="stroke-current text-blue-600 dark:text-blue-500" stroke-width="1.5" stroke-dasharray='90 100' stroke-linecap="round"></circle>
+              <circle cx="18" cy="18" r="16" fill="none" className="stroke-current text-blue-600 dark:text-blue-500" stroke-width="1.5" stroke-dasharray='1 100' stroke-linecap="round"></circle>
             </svg>
 
             <div className="absolute top-1/2 start-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
